90e1ab4dc4700ff6703a72983acf857fe009d1df
Alex Ganagin <ganagin@gmail.com>
2019-03-28 12:48:45 +0700

    Added annotations for model Link

--- models/Link.php
+++ models/Link.php
@@ -4,6 +4,13 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
+/**
+ * Model for table "link".
+ *
+ * @property integer $id
+ * @property string $short
+ * @property string $long
+ */
 class Link extends ActiveRecord
 {
 }
